4172ece8c4c9e4e7abe27956f468b63f074055b6
marcus.santos <marcus.santos@g.globo>
2022-04-25 14:09:54 -0300

    Implemented doScale and doTranslation

--- src/script.js
+++ src/script.js
@@ -1,124 +1,6 @@
 import * as THREE from "three";
 import "./style.css";
 
-// Transformações matriciais
-var m4 = {
-  // Multiplicação das matrizes
-  multiply: function (a, b) {
-    console.log(a);
-    console.log(b);
-    console.log(a[0]);
-    var a00 = a[0 * 4 + 0];
-    var a01 = a[0 * 4 + 1];
-    var a02 = a[0 * 4 + 2];
-    var a03 = a[0 * 4 + 3];
-    var a10 = a[1 * 4 + 0];
-    var a11 = a[1 * 4 + 1];
-    var a12 = a[1 * 4 + 2];
-    var a13 = a[1 * 4 + 3];
-    var a20 = a[2 * 4 + 0];
-    var a21 = a[2 * 4 + 1];
-    var a22 = a[2 * 4 + 2];
-    var a23 = a[2 * 4 + 3];
-    var a30 = a[3 * 4 + 0];
-    var a31 = a[3 * 4 + 1];
-    var a32 = a[3 * 4 + 2];
-    var a33 = a[3 * 4 + 3];
-    var b00 = b[0 * 4 + 0];
-    var b01 = b[0 * 4 + 1];
-    var b02 = b[0 * 4 + 2];
-    var b03 = b[0 * 4 + 3];
-    var b10 = b[1 * 4 + 0];
-    var b11 = b[1 * 4 + 1];
-    var b12 = b[1 * 4 + 2];
-    var b13 = b[1 * 4 + 3];
-    var b20 = b[2 * 4 + 0];
-    var b21 = b[2 * 4 + 1];
-    var b22 = b[2 * 4 + 2];
-    var b23 = b[2 * 4 + 3];
-    var b30 = b[3 * 4 + 0];
-    var b31 = b[3 * 4 + 1];
-    var b32 = b[3 * 4 + 2];
-    var b33 = b[3 * 4 + 3];
-    console.log(a00);
-    console.log(a01);
-    return [
-      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
-      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
-      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
-      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
-      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
-      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
-      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
-      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
-      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
-      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
-      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
-      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
-      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
-      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
-      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
-      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
-    ];
-  },
-
-  // Translação
-  translation: function (tx, ty, tz) {
-    return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, tx, ty, tz, 1];
-  },
-
-  // Rotação eixo x
-  xRotation: function (angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-
-    return [1, 0, 0, 0, 0, c, s, 0, 0, -s, c, 0, 0, 0, 0, 1];
-  },
-
-  // Rotação eixo y
-  yRotation: function (angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-
-    return [c, 0, -s, 0, 0, 1, 0, 0, s, 0, c, 0, 0, 0, 0, 1];
-  },
-
-  // Rotação eixo z
-  zRotation: function (angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-
-    return [c, s, 0, 0, -s, c, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-  },
-
-  // Transformação escalar
-  scaling: function (sx, sy, sz) {
-    return [sx, 0, 0, 0, 0, sy, 0, 0, 0, 0, sz, 0, 0, 0, 0, 1];
-  },
-
-  // Funções das transformações matriciais
-  translate: function (m, tx, ty, tz) {
-    return m4.multiply(m, m4.translation(tx, ty, tz));
-  },
-
-  xRotate: function (m, angleInRadians) {
-    return m4.multiply(m, m4.xRotation(angleInRadians));
-  },
-
-  yRotate: function (m, angleInRadians) {
-    return m4.multiply(m, m4.yRotation(angleInRadians));
-  },
-
-  zRotate: function (m, angleInRadians) {
-    return m4.multiply(m, m4.zRotation(angleInRadians));
-  },
-
-  scale: function (m, sx, sy, sz) {
-    // console.log(m4.multiply(m, m4.scaling(sx, sy, sz)));
-    return m4.multiply(m, m4.scaling(sx, sy, sz));
-  },
-};
-
 // Variáveis de ambiente
 let scene,
   camera,
@@ -169,6 +51,7 @@ let smoothing = 10;
 let mousePos = { x: 0, y: 0 };
 let halfPI = Math.PI / 2;
 
+// HANDLERS
 const onWindowResize = () => {
   sizes.height = window.innerHeight;
   sizes.width = window.innerWidth;
@@ -214,6 +97,29 @@ const updateSpeed = () => {
   speed.y = (mousePos.y - windowHalfY) / 10;
 };
 
+// TRANSFORMAÇÕES MATRICIAIS
+const doTranslation = (x, y, z) => {
+  const translationMatrix = new THREE.Matrix4();
+  translationMatrix.set(
+    1, 0, 0, x,
+    0, 1, 0, y,
+    0, 0, 1, z,
+    0, 0, 0, 1
+  );
+  return translationMatrix;
+}
+
+const doScale = (x, y, z) => {
+  var scaleMatrix = new THREE.Matrix4();
+  scaleMatrix.set(
+    x, 0, 0, 0,
+    0, y, 0, 0,
+    0, 0, z, 0,
+    0, 0, 0, 1
+  );
+  return scaleMatrix;
+}
+
 // LUZES
 // Uma Luz hemisphere para dar visão ambiental global
 // E uma segunda para dar algumas sombras
@@ -221,14 +127,7 @@ const createLight = () => {
   light = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.3);
   scene.add(light);
   shadowLight = new THREE.DirectionalLight(0xffffff, 0.8);
-  console.log(shadowLight);
-  shadowLight.matrix.elements = m4.translate(
-    shadowLight.matrix.elements,
-    1,
-    1,
-    1
-  );
-  // shadowLight.position.set(1, 1, 1);
+  shadowLight.applyMatrix4(doTranslation(1, 1, 1));
   scene.add(shadowLight);
 };
 
@@ -537,36 +436,24 @@ const createPlane = () => {
   });
 
   wingsPlane = new THREE.Mesh(wingGeom, wingMat);
-  console.log(wingsPlane);
-  // wingsPlane.scale.set(8, 1, 0.1);
-  wingsPlane.matrix.elements = m4.scale(wingsPlane.matrix.elements, 8, 1, 0.1);
-  console.log(wingsPlane);
+  wingsPlane.applyMatrix4(doScale(8, 1, 0.1));
+  wingsPlane.applyMatrix4(doTranslation(0, 0, 0));
 
   wingsPlane.rotation.x = halfPI;
   wingsPlane.rotation.z = -halfPI;
 
-  //wingsPlane.matrix.x.set(m4.xRotate(wingsPlane.matrix, halfPI));
-
-  wingsPlane.position.x = 0;
-  wingsPlane.position.y = 0;
-  wingsPlane.position.z = 0;
-
   // LEME
   rudderPlane = new THREE.Mesh(wingGeom, wingMat);
 
-  rudderPlane.scale.set(1.5, 1, 0.1);
-
-  rudderPlane.position.x = -90;
-  rudderPlane.position.y = 30;
+  rudderPlane.applyMatrix4(doScale(1.5, 1, 0.1));
+  rudderPlane.applyMatrix4(doTranslation(-90, 30, 0));
   rudderPlane.rotation.z = -halfPI;
 
   // ESTABILIZADOR
   stabilizerPlane = new THREE.Mesh(wingGeom, wingMat);
 
-  stabilizerPlane.scale.set(4, 1, 0.1);
-
-  stabilizerPlane.position.x = -90;
-  stabilizerPlane.position.y = -10;
+  stabilizerPlane.applyMatrix4(doScale(4, 1, 0.1));
+  stabilizerPlane.applyMatrix4(doTranslation(-90, -10, 0));
   stabilizerPlane.rotation.x = halfPI;
   stabilizerPlane.rotation.z = -halfPI;
 
@@ -575,9 +462,9 @@ const createPlane = () => {
   let windowMat = new THREE.MeshLambertMaterial({
     color: 0x00abff,
   });
+
   windowPlane = new THREE.Mesh(windowGeom, windowMat);
-  windowPlane.position.x = 30;
-  windowPlane.position.y = 40;
+  windowPlane.applyMatrix4(doTranslation(30, 40, 0));
 
   //HÉLICE
   let propellerGeom = createCube(5, 120, 15);
@@ -585,7 +472,7 @@ const createPlane = () => {
     color: 0xdc143c,
   });
   let prop1 = new THREE.Mesh(propellerGeom, propeller);
-  prop1.position.x = 122;
+  prop1.applyMatrix4(doTranslation(122, 0, 0));
 
   let prop2 = prop1.clone();
   prop2.rotation.x = Math.PI / 2;
@@ -654,11 +541,14 @@ const getParticle = () => {
 const flyParticle = () => {
   let particle = getParticle();
   // Seleciona a posição da particula de forma randômica, mas a inicia fora do campo de visão e a dá uma escala também randômica
-  particle.position.x = xLimit;
-  particle.position.y = Math.random() * yLimit * 2 - yLimit;
-  particle.position.z = Math.random() * maxParticlesZ;
+  particle.applyMatrix4(doTranslation(
+                                      xLimit, 
+                                      Math.random() * yLimit * 2 - yLimit, 
+                                      Math.random() * maxParticlesZ
+                                    ));
+
   let scale = 0.2 + Math.random();
-  particle.scale.set(scale, scale, scale);
+  particle.applyMatrix4(doScale(scale, scale, scale));
   flyingParticles.push(particle);
   scene.add(particle);
 };
@@ -678,7 +568,7 @@ const init = () => {
     nearPlane,
     farPlane
   );
-  camera.position.z = 1000;
+  camera.applyMatrix4(doTranslation(0, 0, 1000));
 
   // Cria o canvas que renderiza o WebGL dentro da div background
   canvas = document.querySelector("canvas.webgl");
@@ -762,7 +652,7 @@ const loop = () => {
   bodyPlane.material.color.setRGB(rvalue, gvalue, bvalue);
 
   // O avião estica quando está mais rápido, para dar uma impressão de velocidade e dilatação térmica
-  plane.scale.set(1 + v2, 1 - v2, 1 - v2);
+  plane.applyMatrix4(doScale(1 + v2, 1 - v2, 1 - v2));
 
   // Atualização das particulas
   for (let i = 0; i < flyingParticles.length; i++) {
